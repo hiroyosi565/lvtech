aeb9a73ba83e714def6a4e33ed06d177ee68611f
hiroyosi565 <hiroyosi565@gmail.com>
2022-01-18 22:12:23 +0900

    [add]記事一覧画面追加

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function() {
+    return view('posts/index');
 });
+
+Route::get('/', 'PostController@index');
