72c2c0970ee2a95d98ee38cad1a707293bc77d71
hiroyosi565 <hiroyosi565@gmail.com>
2022-01-21 23:28:44 +0900

    [add]記事更新画面

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -11,6 +12,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index');
+Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
+Route::get('/posts/{post}', 'PostController@show');
+Route::post('/posts', 'PostController@store');
