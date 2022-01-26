da960b23704799c7ec41b4bde50abeab0800a78d
hiroyosi565 <hiroyosi565@gmail.com>
2022-01-26 18:07:51 +0900

    [add]カテゴリ画面表示

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -10,11 +10,27 @@
     </head>
     <body>
         <h1>Blog Name</h1>
+        <p class='edit'>[<a href='/posts/{{ $post->id }}/edit'>更新</a>]</p>
+        <form action="/posts/{{ $post->id }}" id="form_delete" method="post">
+            {{ csrf_field() }}
+            {{ method_field('delete')}}
+            <input type="submit" style="display:none">
+            <p class='delete'>[<span onclick="return deletePost(this);">削除</span>]</p>
+        </form>
         <div class='post'>
             <h2 class='title'>{{ $post->title }}</h2>
             <p clas='body'>{{ $post->body }}</p>
             <p class='updated_at'>{{ $post->updated_at }}</p>
         </div>
+        <a href="/categories/{{ $post->category->id }}">{{ $post->category->name }}</a>
         <div class="back">[<a href="/">back</a>]</div>
+        <script>
+            function deletePost(e){
+                'use strict'
+                if (confirm('削除すると復元できません. \n本当に削除しますか? ')){
+                    document.getElementById('form_delete').submit();
+                }
+            }
+        </script>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +14,9 @@
 
 Route::get('/', 'PostController@index');
 Route::get('/posts/create', 'PostController@create');
+Route::get('/posts/{post}/edit', 'PostController@edit');
+Route::put('/posts/{post}', 'PostController@update');
+Route::delete('/posts/{post}', 'PostController@destory');
 Route::get('/posts/{post}', 'PostController@show');
-
 Route::post('/posts', 'PostController@store');
+Route::get('/categories/{category}', 'CategoryController@index');
